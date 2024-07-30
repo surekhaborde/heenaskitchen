e7a0b30c9fc3ae0bea33433bb3eee4b1b24e09a7
Surekha Borde <surekha.sumb13@gmail.com>
2024-07-30 18:28:25 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Restuarant from './Component/Basics/Restuarant'
+
+const App = () => {
+  return (
+    <div>
+      
+      <Restuarant></Restuarant>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Component/Basics/menuApi.js
@@ -0,0 +1,113 @@
+const Menu = [
+    {
+      id: 1,
+      image: "images/maggie.jpeg",
+      name: "Maggi",
+      category: "breakfast",
+      price: "12₹",
+      description:
+        "I love Maggi really oo yues  Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+  
+    {
+      id: 2,
+      image: "images/aalo_pakoda.jpeg",
+      name: "Allu pakoda",
+      category: "evening",
+      price: "20₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 3,
+      image: "images/Corn.jpeg",
+      name: "corn",
+      category: "breakfast",
+      price: "10₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 4,
+      image: "../images/dosa.jpeg",
+      name: "chola",
+      category: "lunch",
+      price: "50₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 5,
+      image: "../images/pizza.jpeg",
+      name: "pizza",
+      category: "dinner",
+      price: "80₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 6,
+      image: "../images/veg thali.jpeg",
+      name: "Veg Thali",
+      category: "dinner",
+      price: "180₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 7,
+      image: "../images/rajma_chawal.jpeg",
+      name: "Rajma Chawal",
+      category: "dinner",
+      price: "60₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 8,
+      image: "../images/puri_bhaji.jpeg",
+      name: "Puri bhaji",
+      category: "lunch",
+      price: "60₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 9,
+      image: "../images/samosa.jpeg",
+      name: "Samaso",
+      category: "breakfast",
+      price: "10₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+        id: 10,
+        image: "../images/pavbhaji.jpeg",
+        name: "Pavbhaji",
+        category: "lunch",
+        price: "100₹",
+        description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+      },
+      {
+        id: 11,
+        image: "../images/idli.jpeg",
+        name: "Idli",
+        category: "evening",
+        price: "50₹",
+        description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+      },
+      {
+        id: 12,
+        image: "../images/vadapav.jpeg",
+        name: "Vadapav",
+        category: "evening",
+        price: "50₹",
+        description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+      },
+  ];
+  
+  export default Menu;
